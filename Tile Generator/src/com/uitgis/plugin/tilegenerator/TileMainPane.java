90c5253b30cabff9ed0896c8d7bb679bda362a89
thaihv <thaihv@gmail.com>
2018-11-13 09:56:26 +0700

    clean

--- Tile Generator/src/com/uitgis/plugin/tilegenerator/TileMainPane.java
+++ Tile Generator/src/com/uitgis/plugin/tilegenerator/TileMainPane.java
@@ -2,9 +2,10 @@ package com.uitgis.plugin.tilegenerator;
 
 import java.io.IOException;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.uitgis.plugin.tilegenerator.controller.WizardModule;
+import com.uitgis.plugin.tilegenerator.model.WizardData;
 import com.uitgis.sdk.controls.MapControl;
 
 import javafx.fxml.FXMLLoader;
@@ -15,24 +16,28 @@ import javafx.scene.layout.BorderPane;
 public class TileMainPane extends BorderPane {
 
 	public TileMainPane(MapControl mapControl) {
-		
-		final Injector injector = Guice.createInjector( new WizardModule() );
+
+		final Injector injector = Guice.createInjector(new AbstractModule() {
+
+			@Override
+			protected void configure() {
+				WizardData model = new WizardData();
+				bind(WizardData.class).toInstance(model);
+
+			}
+
+		});
 
 		Parent p;
 		try {
-			p = FXMLLoader.load( TileMainPane.class.getResource("/fxml/Wizard.fxml"),
-											  null,
-											  new JavaFXBuilderFactory(),
-											  (clazz) -> injector.getInstance(clazz));
+			p = FXMLLoader.load(TileMainPane.class.getResource("/fxml/Wizard.fxml"), null, new JavaFXBuilderFactory(),
+					(clazz) -> injector.getInstance(clazz));
 			setCenter(p);
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-	
-		
-		
 	}
 
 }
